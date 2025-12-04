9b52ab17f25e53a51e0e3c733d806f0992925e9c
Andres Cruz <desarrollolibre.net@gmail.com>
2025-12-04 13:29:15 -0400

    migraciones

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from elements.models import Element
+
+
 # Create your models here.
+
+class Comment(models.Model):
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    # element = models.ForeignKey('elements.Element', on_delete=models.SET_NULL, null=True)
+    element = models.ForeignKey(Element, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Comment for {self.element.title if self.element else 'N/A'} - {self.created_at.strftime('%Y-%m-%d')}"
+
+
+
+
